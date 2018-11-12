8c74a2f35ab2d11b98984e5ccb2eeffa3004c711
Mohamed lahri <mokalahri@gmail.com>
2018-11-12 11:56:18 +0100

    modifications pour la quete fichier json

--- WeatherParse.java
+++ WeatherParse.java
@@ -25,16 +25,35 @@ public class WeatherParse {
         }
         
         // TODO parser le fichier
-
+	JSONParser parser = new JSONParser();
+	Object jsonParsed = null;
+	try {
+    	jsonParsed = parser.parse(jsonFile);
+	} 	catch (ParseException | IOException e) {
+   	e.printStackTrace();
+	}
         // TODO récupérer la racine du document
+	JSONObject root = (JSONObject) jsonParsed;
 
         // TODO afficher la valeur de l'attribut "name" de la racine
-
+	String name = (String) root.get("name");
+	System.out.println(name);
+ 
         // TODO afficher les valeurs des attributs "lat" et "lon" de l'élément "coord" contenu dans la racine
+	JSONObject coord = (JSONObject) root.get("coord");	
+	Double lat = (Double) coord.get("lat");
+	Double lon = (Double) coord.get("lon");
+	System.out.println(lat);
+	System.out.println(lon);
 
         // TODO parcourir tous les éléments de "weather" et afficher le contenu de "main"
-
-        /*
+	JSONArray weather = (JSONArray) root.get("weather");
+	for (int i = 0; i < weather.size(); i++) {
+    	JSONObject weatherobject = (JSONObject) weather.get(i);
+	String main = (String) weatherobject.get("main");
+    	System.out.println(main);
+	}
+        /*x
             Résultat attendu :
             * City name: London
             * City latitude: 51.51
